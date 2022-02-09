3089131ea9849142f7b957de01e3413bd67c0d74
Arun Sood <arsood@gmail.com>
2022-02-08 18:49:15 -0800

    Form fields for create new event

--- app/javascript/pages/events.js
+++ app/javascript/pages/events.js
@@ -1,7 +1,17 @@
 import { Dropzone } from "dropzone";
+import Datepicker from "../../../vendor/js/datepicker/datepicker";
 
 document.addEventListener("DOMContentLoaded", function() {
   if (document.querySelector(".dropzone")) {
     const dropzone = new Dropzone(".dropzone", { url: "/file/post", maxFiles: 4 });
+  }
+
+  if (document.querySelector(".datepicker")) {
+    const datepicker = new Datepicker(".datepicker", {
+      time: true,
+      onChange: (dateTime) => {
+        console.log(dateTime)
+      }
+    });
   }
 });
