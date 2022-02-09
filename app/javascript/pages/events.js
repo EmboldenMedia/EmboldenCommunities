c048e153f9d4dcd739f32fca470c4e98ad973e7a
Arun Sood <arsood@gmail.com>
2022-02-09 10:08:37 -0800

    Set hidden field to selected date

--- app/javascript/pages/events.js
+++ app/javascript/pages/events.js
@@ -1,19 +1,28 @@
 import { Dropzone } from "dropzone";
 import Datepicker from "../../../vendor/js/datepicker/datepicker";
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   if (document.querySelector(".dropzone")) {
-    const authenticityToken = document.querySelector("input[name='authenticity_token']").value;
-    
-    const dropzone = new Dropzone(".dropzone", { url: `/events?authenticity_token=${authenticityToken}`, maxFiles: 4 });
+    const authenticityToken = document.querySelector(
+      "input[name='authenticity_token']"
+    ).value;
+
+    const dropzone = new Dropzone(".dropzone", {
+      url: `/events?authenticity_token=${authenticityToken}`,
+      maxFiles: 4,
+    });
   }
 
   if (document.querySelector(".datepicker")) {
     const datepicker = new Datepicker(".datepicker", {
       time: true,
       onChange: (dateTime) => {
-        console.log(dateTime)
-      }
+        if (dateTime) {
+          const isoDate = new Date(dateTime).toISOString();
+
+          document.querySelector("input[name='event_date']").value = isoDate;
+        }
+      },
     });
   }
 });
